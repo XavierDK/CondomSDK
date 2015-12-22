4fb449d9c0b639a4215054d308737964f21166ca
Xavier De Koninck <xdekoninck.externe@pagesjaunes.fr>
2015-12-22 14:16:22 +0100

    Initial Commit

---
+++ CondomSDK/CondomSDK.h
@@ -0,0 +1,19 @@
+//
+//  CondomSDK.h
+//  CondomSDK
+//
+//  Created by Xavier De Koninck on 22/12/2015.
+//  Copyright © 2015 PagesJaunes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CondomSDK.
+FOUNDATION_EXPORT double CondomSDKVersionNumber;
+
+//! Project version string for CondomSDK.
+FOUNDATION_EXPORT const unsigned char CondomSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CondomSDK/PublicHeader.h>
+
+
